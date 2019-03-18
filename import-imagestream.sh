1cfce740f419016f71005bd21c43e4e5d3c5de21
rlucente <rlucente@redhat.com>
2019-03-18 09:00:50 -0400

    make sure to login to minishift

--- import-imagestream.sh
+++ import-imagestream.sh
@@ -10,7 +10,7 @@ oc logout
 eval $(minishift docker-env)
 IMAGEID=$(docker load -i $APP\@latest.tar.gz | grep sha256 | awk '{print $4}')
 
-oc login -u admin -p admin
+oc login -u admin -p admin $(minishift ip):8443
 docker login -u admin -p $(oc whoami -t) 172.30.1.1:5000
 docker tag $IMAGEID 172.30.1.1:5000/$PROJECT/$APP
 docker push 172.30.1.1:5000/$PROJECT/$APP
